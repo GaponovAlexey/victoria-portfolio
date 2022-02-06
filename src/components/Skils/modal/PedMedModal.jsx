9de26e65818152456a6b644879d631819cf699ca
Kekelberi <alexei.gaponov@gmail.com>
2022-02-06 17:49:13 +0300

    save

--- src/components/Skils/modal/PedMedModal.jsx
+++ src/components/Skils/modal/PedMedModal.jsx
@@ -7,9 +7,9 @@ import { isModalMedReducer } from '../../../Redux/reducer'
 export const PedMedModal = () => {
   const { isModalMed } = useSelector((store) => store.data)
   const dispatch = useDispatch()
-  const [nav1, setNav1] = useState();
-  const [nav2, setNav2] = useState();
-  
+  const [nav1, setNav1] = useState()
+  const [nav2, setNav2] = useState()
+
   return (
     <div>
       <div
@@ -25,55 +25,55 @@ export const PedMedModal = () => {
       >
         <div>MODAL MED</div>
         <div>
-        <h2>Slider Syncing (AsNavFor)</h2>
-      <h4>First Slider</h4>
-      <Slider asNavFor={nav2} ref={(slider1) => setNav1(slider1)}>
-        <div>
-          <h3>1</h3>
-        </div>
-        <div>
-          <h3>2</h3>
-        </div>
-        <div>
-          <h3>3</h3>
-        </div>
-        <div>
-          <h3>4</h3>
-        </div>
-        <div>
-          <h3>5</h3>
-        </div>
-        <div>
-          <h3>6</h3>
-        </div>
-      </Slider>
-      <h4>Second Slider</h4>
-      <Slider
-        asNavFor={nav1}
-        ref={(slider2) => setNav2(slider2)}
-        slidesToShow={3}
-        swipeToSlide={true}
-        focusOnSelect={true}
-      >
-        <div>
-          <h3>1</h3>
-        </div>
-        <div>
-          <h3>2</h3>
-        </div>
-        <div>
-          <h3>3</h3>
-        </div>
-        <div>
-          <h3>4</h3>
-        </div>
-        <div>
-          <h3>5</h3>
-        </div>
-        <div>
-          <h3>6</h3>
-        </div>
-      </Slider>
+          <h2>Slider Syncing (AsNavFor)</h2>
+          <h4>First Slider</h4>
+          <Slider asNavFor={nav2} ref={(slider1) => setNav1(slider1)}>
+            <div>
+              <h3>1</h3>
+            </div>
+            <div>
+              <h3>2</h3>
+            </div>
+            <div>
+              <h3>3</h3>
+            </div>
+            <div>
+              <h3>4</h3>
+            </div>
+            <div>
+              <h3>5</h3>
+            </div>
+            <div>
+              <h3>6</h3>
+            </div>
+          </Slider>
+          <h4>Second Slider</h4>
+          <Slider
+            asNavFor={nav1}
+            ref={(slider2) => setNav2(slider2)}
+            slidesToShow={4}
+            swipeToSlide={true}
+            focusOnSelect={true}
+          >
+            <div>
+              <h3>1</h3>
+            </div>
+            <div>
+              <h3>2</h3>
+            </div>
+            <div>
+              <h3>3</h3>
+            </div>
+            <div>
+              <h3>4</h3>
+            </div>
+            <div>
+              <h3>5</h3>
+            </div>
+            <div>
+              <h3>6</h3>
+            </div>
+          </Slider>
         </div>
         <div>
           <div
