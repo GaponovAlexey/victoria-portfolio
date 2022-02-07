05f59a181c45a79b745efec0aba5ce3e53cc8f4c
Kekelberi <alexei.gaponov@gmail.com>
2022-02-07 18:37:34 +0300

    save

--- src/components/coments/ComentsHome.jsx
+++ src/components/coments/ComentsHome.jsx
@@ -1,183 +1,68 @@
 import { css } from '@emotion/css'
 import React from 'react'
+import { useSelector } from 'react-redux'
 import Slider from 'react-slick'
 
-
-
 export const ComentsHome = () => {
+  const { comentData } = useSelector((state) => state.data)
   const settings = {
     dots: false,
-    slidesToShow: 4,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    // adaptiveHeight: true, 
+    infinite: true,
+    slidesToShow: 3,
+    // autoplay: true,
+    // autoplaySpeed: 2000,
     slidesToScroll: 1,
     vertical: true,
     verticalSwiping: true,
     swipeToSlide: true,
     // fade: 3,
-    // infinite: true,
-    focusOnSelect: true,
+    // focusOnSelect: true,
     initialSlide: true,
   }
 
   return (
-    <div className={css`margin-bottom: 40px;`}>
+    <div
+      className={css`
+        margin-bottom: 60px;
+        background-color:  #F6F6F6;
+      `}
+    >
       <h3
         className={css`
           font-family: GilroyBold;
           font-size: 34px;
           line-height: 42px;
           text-align: center;
+          margin-bottom: 30px;
         `}
       >
         Comments
       </h3>
       <div>
         <Slider {...settings}>
-          <div>
-            <h3>
-              <img
+          {comentData.map((coment) => (
+            <div key={coment.user}>
+              <div
                 className={css`
-                  width: 100%;
-                  height: 100%;
+                  display: flex;
+                  justify-content: space-between;
+                  margin-bottom: 10px;
+                  font-size: 18px;
+                  line-height: 22px;
                 `}
-                src={com1}
-                alt='com1'
-              />
-            </h3>
-          </div>
-          <div>
-            <img
-              className={css`
-                width: 100%;
-                height: 100%;
-              `}
-              src={com2}
-              alt='com2'
-            />
-          </div>
-          <div>
-            <img
-              className={css`
-                width: 100%;
-                height: 100%;
-              `}
-              src={com3}
-              alt='com3'
-            />
-          </div>
-          <div>
-            <img
-              className={css`
-                width: 100%;
-                height: 100%;
-              `}
-              src={com4}
-              alt='com4'
-            />
-          </div>
-          <div>
-            <img
-              className={css`
-                width: 100%;
-                height: 100%;
-              `}
-              src={com5}
-              alt='com5'
-            />
-          </div>
-          <div>
-            <img
-              className={css`
-                width: 100%;
-                height: 100%;
-              `}
-              src={com6}
-              alt='com6'
-            />
-          </div>
-          <div>
-            <img
-              className={css`
-                width: 100%;
-                height: 100%;
-              `}
-              src={com7}
-              alt='com7'
-            />
-          </div>
-          <div>
-            <img
-              className={css`
-                width: 100%;
-                height: 100%;
-              `}
-              src={com8}
-              alt='com8'
-            />
-          </div>
-          <div>
-            <img
-              className={css`
-                width: 100%;
-                height: 100%;
-              `}
-              src={com9}
-              alt='com9'
-            />
-          </div>
-          <div>
-            <img
-              className={css`
-                width: 100%;
-                height: 100%;
-              `}
-              src={com10}
-              alt='com10'
-            />
-          </div>
-          <div>
-            <img
-              className={css`
-                width: 100%;
-                height: 100%;
-              `}
-              src={com11}
-              alt='com11'
-            />
-          </div>
-          <div>
-            <img
-              className={css`
-                width: 100%;
-                height: 100%;
-              `}
-              src={com12}
-              alt='com12'
-            />
-          </div>
-          <div>
-            <img
-              className={css`
-                width: 100%;
-                height: 100%;
-              `}
-              src={com13}
-              alt='com13'
-            />
-          </div>
-          <div>
-            <img
-              className={css`
-                width: 100%;
-                height: 100%;
-              `}
-              src={com14}
-              alt='com14'
-            />
-          </div>
-          
+              >
+                <div className={css``}>{coment.coment}</div>
+                <div
+                  className={css`
+                    margin-left: 30px;
+                    font-family: GilroyBold;
+                  `}
+                >
+                  {coment.user}
+                </div>
+              </div>
+            </div>
+          ))}
         </Slider>
       </div>
     </div>
