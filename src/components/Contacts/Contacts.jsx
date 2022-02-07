114e26c8096bcc6a6cfa63dc2f24130dda0f09a1
Kekelberi <alexei.gaponov@gmail.com>
2022-02-07 20:30:19 +0300

    save

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,10 +1,60 @@
+import { css } from '@emotion/css'
 import React from 'react'
 
 export const Contacts = () => {
   return (
     <div>
       <div>
-        <h2>Contacts</h2>
+        <h2
+          className={css`
+            padding-top: 114px;
+            font-family: GilroyBold;
+            font-size: 34px;
+            line-height: 42px;
+            text-align: center;
+          `}
+        >
+          Contacts
+        </h2>
+        <div
+          className={css`
+            font-size: 18px;
+            line-height: 22px;
+            text-align: center;
+          `}
+        >
+          <div
+            className={css`
+              width: 273px;
+              margin: 0 auto;
+              margin-top: 20px;
+            `}
+          >
+            Want to know more or just chat? You are welcome!
+          </div>
+          <div
+            className={css`
+              display: inline-block;
+              background-color: black;
+              border-radius: 50px;
+              margin-top: 30px;
+            `}
+          >
+            <div
+              className={css`
+                width: 180.5px;
+                height: 42px;
+                justify-content: center;
+                display: flex;
+                flex-wrap: wrap;
+                align-items: center;
+                color: white;
+              `}
+            >
+              Send message
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
