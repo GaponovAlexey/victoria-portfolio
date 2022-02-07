0af1f95701fec803910f6ea0dd664cd203f523c7
Kekelberi <alexei.gaponov@gmail.com>
2022-02-07 15:28:30 +0300

    save

--- src/components/coments/ComentsHome.jsx
+++ src/components/coments/ComentsHome.jsx
@@ -1,7 +1,26 @@
 import { css } from '@emotion/css'
 import React from 'react'
+import Slider from 'react-slick'
+
+import com1 from '../../img/koments/1.jpg'
 
 export const ComentsHome = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    vertical: true,
+    verticalSwiping: true,
+    swipeToSlide: true,
+    beforeChange: function (currentSlide, nextSlide) {
+      console.log('before change', currentSlide, nextSlide)
+    },
+    afterChange: function (currentSlide) {
+      console.log('after change', currentSlide)
+    },
+  }
+
   return (
     <div>
       <h3
@@ -14,6 +33,42 @@ export const ComentsHome = () => {
       >
         Comments
       </h3>
+      <div>
+        <Slider {...settings}>
+          <div>
+            <h3>
+              <img
+                className={css`
+                  width: 100%;
+                  height: 100%;
+                `}
+                src={com1}
+                alt='com1'
+              />
+            </h3>
+          </div>
+          <div>
+            <h3>2</h3>
+            <img src={com1} alt='com1' />
+          </div>
+          <div>
+            <h3>3</h3>
+            <img src={com1} alt='com1' />
+          </div>
+          <div>
+            <h3>4</h3>
+            <img src={com1} alt='com1' />
+          </div>
+          <div>
+            <h3>5</h3>
+            <img src={com1} alt='com1' />
+          </div>
+          <div>
+            <h3>6</h3>
+            <img src={com1} alt='com1' />
+          </div>
+        </Slider>
+      </div>
     </div>
   )
 }
