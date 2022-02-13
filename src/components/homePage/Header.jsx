bbd412df2beb06aae40839b92dd3f42ef5d526f8
Kekelberi <alexei.gaponov@gmail.com>
2022-02-13 19:56:03 +0300

    save

--- src/components/homePage/Header.jsx
+++ src/components/homePage/Header.jsx
@@ -1,17 +1,18 @@
 import { css } from '@emotion/css'
 import React, { useState } from 'react'
-
 export const Header = () => {
   const [Isactive, setIsactive] = useState(false)
 
   const isShow = css`
-    display: block;
-    width: 100%;
-    height: 2px;
-    background-color: red;
-    position: absolute;
-    left: 0;
-    ${Isactive && `display: none;`}
+    @media (max-width: 767px) {
+      display: block;
+      width: 100%;
+      height: 2px;
+      background-color: red;
+      position: absolute;
+      left: 0;
+      ${Isactive && `display: none;`}
+    }
   `
 
   const burger__liner_first = css`
@@ -24,50 +25,106 @@ export const Header = () => {
   const burger__liner_third = css`
     bottom: 0;
   `
-
+  const hideElementmenu = css`
+    display: none; ;
+  `
+  const headerItem = css`
+    ${Isactive &&
+    `width: 100%;
+      display: flex;
+      flex-wrap: wrap;
+      font-size: 52px;
+      line-height: 48px;
+      margin-bottom: 30px;
+    `}
+  `
   return (
     <header
       className={css`
         position: fixed;
-        display: flex;
-        flex-wrap: wrap;
-        justify-content: space-between;
-        width: 960px;
-        margin: 0 auto;
-        padding: 33px 0px 32px 0px;
-        border-bottom: 1px solid #828282;
-        background-color: rgba(255, 255, 255, 0.3);
         z-index: 20;
+
+        ${Isactive &&
+        `@media (max-width: 767px) {
+          position: fixed;
+          width: 100%;
+          height: 100%;
+          top: 0;
+          left: 0;
+          background-color: black;
+          color: #fff;
+          padding: 0 75px;
+        }`}
       `}
     >
-      <li>Home</li>
-      <li>About</li>
-      <li>Skills</li>
-      <li>Comments</li>
-      <li>Certificates</li>
-      <li>Contacts</li>
+      {Isactive && (
+        <div
+          onClick={() => setIsactive(false)}
+          className={css`
+            padding-left: 60%;
+            font-size: 40px;
+          `}
+        >
+          &#10006;
+        </div>
+      )}
       <div
         className={css`
-          width: 40px;
-          height: 20px;
-          position: relative;
+          justify-content: space-around;
+          align-items: center;
+          width: 960px;
+          margin: 0 auto;
+          border-bottom: 1px solid #828282;
+          background-color: rgba(255, 255, 255, 0.6);
+
+          ${!Isactive &&
+          `@media (max-width: 767px) {
+            display: none;
+          }`}
+          display: flex;
+          flex-wrap: wrap;
+          padding: 33px 0px 32px 0px;
+          border-bottom: none;
+          background-color: rgba(255, 255, 255, 0);
         `}
       >
-        <span
+        <li className={headerItem}>Home</li>
+        <li className={headerItem}>About</li>
+        <li className={headerItem}>Skills</li>
+        <li className={headerItem}>Comments</li>
+        <li className={headerItem}>Certificates</li>
+        <li className={headerItem}>Contacts</li>
+      </div>
+      <div>
+        <div
+          onClick={() => setIsactive(true)}
           className={css`
-            ${isShow} ${burger__liner_first}
+            display: inline-block;
+            margin-left: 60px;
+            top: 4px;
+            @media (max-width: 767px) {
+              width: 20px;
+              height: 20px;
+              position: relative;
+            }
           `}
-        ></span>
-        <span
-          className={css`
-            ${isShow} ${burger__liner_second}
-          `}
-        ></span>
-        <span
-          className={css`
-            ${isShow} ${burger__liner_third}
-          `}
-        ></span>
+        >
+          <span
+            className={css`
+              ${isShow} ${burger__liner_first}
+            `}
+          ></span>
+          <span
+            className={css`
+              ${isShow} ${burger__liner_second}
+            `}
+          ></span>
+          <span
+            className={css`
+              ${isShow} ${burger__liner_third}
+            `}
+          ></span>
+        </div>
       </div>
     </header>
   )
